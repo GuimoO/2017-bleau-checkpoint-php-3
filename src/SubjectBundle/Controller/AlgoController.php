7eb38534f0b624825cf4ebaa072b69f87b44a376
Florian Grandjean <florian.pdf@gmail.com>
2017-05-30 14:36:53 +0200

    Ajout de la correction algo

--- src/SubjectBundle/Controller/AlgoController.php
+++ src/SubjectBundle/Controller/AlgoController.php
@@ -11,8 +11,85 @@ class AlgoController extends Controller
     //////////////////////////////////////
     //
 
-    public function dateInterval ($series) {
-
+    /**
+     * [dateInterval description]
+     * Renvoie la plus grande distance entre deux séries
+     * @param  array $series
+     * @return integer
+     */
+/*    public function dateInterval ($series) {
+        ksort($series);
+        $year= 0;
+        $result=[];
+        foreach ($series as $key => $value) {
+            if($year == 0){
+                $year = $key;               
+            }
+            else{
+                $result[]= $key - $year;
+                $year = $key;
+            }
+        }
+        $max = max($result);
+        return $max;
     }
+*/
 
+
+    /**
+     * [dateInterval description]
+     * Renvoie la plus grande distance entre deux séries
+     * @param  array $series
+     * @return integer
+     */
+ /*   public function dateInterval ($series) {
+        ksort($series);
+        $year = $result = 0;
+        foreach ($series as $key => $value) {
+            $result = $year != 0 ? ($key - $year >= $result ? $key - $year : $result) : null;
+            $year = $key;
+        }
+        return $result;
+    }
+*/
+
+    /**
+     * [dateInterval description]
+     * Renvoie la différence max entre deux series, ainsi que le titre et année des deux série
+     * @param  array $series
+     * @return array
+     */
+/*    public function dateInterval ($series) {
+        ksort($series);
+        $year = 0;
+        $maxDiff = 0;
+
+        $previous = array();
+        $current = array();
+        $result = array();
+        $serie = '';
+
+        foreach ($series as $key => $value) {
+            if($year != 0){
+                $diff = $key - $year;
+                if ($diff >= $maxDiff) {
+
+                    $result['previous']['serie'] = $serie;
+                    $result['previous']['year'] = $year;
+                    $result['next']['serie'] = $value;
+                    $result['next']['year'] = $key;
+
+                    $maxDiff = $diff;
+                }
+            }
+            $serie = $value;
+            $year = $key;
+        }
+
+        $result['maxDiff'] = $maxDiff;
+
+        dump($result);
+        return $result;
+    }
+*/
 }
